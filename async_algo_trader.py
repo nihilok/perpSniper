2d5784b7cb48ffa9de75bf900c16e38ab645cacd
nihilok <t0k3n8l4ck@gmail.com>
2021-03-02 15:16:23 +0000

    changed log level (info)

--- async_algo_trader.py
+++ async_algo_trader.py
@@ -14,7 +14,7 @@ from signals import Signals
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
+handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - [ %(levelname)s ] - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
